ed0cf2841f59b99b84b20ba59b16f23f48d14f9c
Tomohiro Suzuki <suzuchan@muf.biglobe.ne.jp>
2020-06-03 00:55:18 +0900

    [WIP] Create ViewModels

---
+++ XFSurfaceDuoSample2020/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XFSurfaceDuoSample2020.Models;
+
+namespace XFSurfaceDuoSample2020.ViewModels
+{
+    public class ItemDetailViewModel : ViewModelBase
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ XFSurfaceDuoSample2020/Views/Master.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XFSurfaceDuoSample2020.ViewModels;
+
+namespace XFSurfaceDuoSample2020
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master 
+    {
+        MasterViewModel viewModel;
+
+        public Master()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new MasterViewModel();
+
+            var obj = viewModel.StationItems[0].Name;
+        }
+    }
+}
